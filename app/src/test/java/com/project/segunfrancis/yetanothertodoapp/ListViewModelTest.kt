8cc6eae8f3e9437d15323c5d6744b64da9dc4dd2
SegunFrancis <francis4dblues@gmail.com>
2020-08-07 07:08:10 +0100

    testing - mock ToDoListViewModel

--- app/src/test/java/com/project/segunfrancis/yetanothertodoapp/ListViewModelTest.kt
+++ app/src/test/java/com/project/segunfrancis/yetanothertodoapp/ListViewModelTest.kt
@@ -1,15 +1,17 @@
 package com.project.segunfrancis.yetanothertodoapp
 
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import com.project.segunfrancis.yetanothertodoapp.data.ToDo
 import com.project.segunfrancis.yetanothertodoapp.data.ToDoRepository
 import com.project.segunfrancis.yetanothertodoapp.ui.list.ToDoListViewModel
-import com.project.segunfrancis.yetanothertodoapp.util.ToDoTestRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
@@ -21,33 +23,18 @@ import org.junit.rules.ExpectedException
 @ExperimentalCoroutinesApi
 class ListViewModelTest {
 
+    private val now = System.currentTimeMillis()
+    private val day = 1000 * 60 * 60 * 24
+
     @get:Rule
     val exceptionRule = ExpectedException.none()
-    private lateinit var repository: ToDoRepository
-
-    @Before
-    fun setUp() {
-        val now = System.currentTimeMillis()
-        val day = 1000 * 60 * 60 * 24
-
-        val toDos = ArrayList<ToDo>()
-        var toDo = ToDo("1", "Todo 1", null, false, now)
-        toDos.add(toDo)
-        toDo = ToDo("2", "Todo 2", now + day, false, now)
-        toDos.add(toDo)
-        toDo = ToDo("3", "Todo 3", now + day, false, now)
-        toDos.add(toDo)
-        toDo = ToDo("4", "Todo 4", now + day, true, now)
-        toDos.add(toDo)
-        toDo = ToDo("5", "Todo 5", now + day, false, now)
-        toDos.add(toDo)
-
-        repository = ToDoTestRepository(toDos)
-    }
 
     @Test
-    fun test_allToDos() {
-        val expected = 5
+    fun test_allToDosEmpty() {
+        val expected = 0
+        val repository: ToDoRepository = mock()
+        whenever(repository.getAllToDos())
+            .thenReturn(flowOf(arrayListOf()))
         val model = ToDoListViewModel(repository)
         val toDos = model.allToDos
 
@@ -60,8 +47,76 @@ class ListViewModelTest {
     }
 
     @Test
-    fun test_upcomingToDosCount() {
+    fun test_allToDosSingle() {
+        val expected = 1
+        val repository: ToDoRepository = mock()
+        whenever(repository.getAllToDos())
+            .thenReturn(
+                flowOf(
+                    arrayListOf(
+                        ToDo("5", "Todo 5", now + day, false, now)
+                    )
+                )
+            )
+        val model = ToDoListViewModel(repository)
+        val toDos = model.allToDos
+
+        assertNotNull(toDos)
+        runBlockingTest {
+            toDos.collect {
+                assertEquals(expected, it.size)
+            }
+        }
+    }
+
+    @Test
+    fun test_allToDosMultiple() {
         val expected = 3
+        val repository: ToDoRepository = mock()
+        whenever(repository.getAllToDos())
+            .thenReturn(
+                flowOf(
+                    arrayListOf(
+                        ToDo("5", "Todo 5", now + day, false, now),
+                        ToDo("3", "Todo 3", now + day, false, now),
+                        ToDo("2", "Todo 2", now + day, false, now)
+                    )
+                )
+            )
+        val model = ToDoListViewModel(repository)
+        val toDos = model.allToDos
+
+        assertNotNull(toDos)
+        runBlockingTest {
+            toDos.collect {
+                assertEquals(expected, it.size)
+            }
+        }
+    }
+
+     @Test
+     fun test_upcomingToDosCountEmpty() {
+         val expected = 0
+         val repository: ToDoRepository = mock()
+         whenever(repository.getUpcomingToDosCount())
+             .thenReturn(flowOf(expected))
+         val model = ToDoListViewModel(repository)
+         val count = model.upcomingToDosCount
+
+         assertNotNull(count)
+         runBlockingTest {
+             count.collect {
+                 assertEquals(expected, it)
+             }
+         }
+     }
+
+    @Test
+    fun test_upcomingToDosCountSingle() {
+        val expected = 1
+        val repository: ToDoRepository = mock()
+        whenever(repository.getUpcomingToDosCount())
+            .thenReturn(flowOf(expected))
         val model = ToDoListViewModel(repository)
         val count = model.upcomingToDosCount
 
@@ -74,10 +129,48 @@ class ListViewModelTest {
     }
 
     @Test
-    fun test_toggleToDo() {
-        val id = "fake"
+    fun test_upcomingToDosCountMultiple() {
+        val expected = 5
+        val repository: ToDoRepository = mock()
+        whenever(repository.getUpcomingToDosCount())
+            .thenReturn(flowOf(expected))
         val model = ToDoListViewModel(repository)
-        exceptionRule.expect(NotImplementedError::class.java)
+        val count = model.upcomingToDosCount
+
+        assertNotNull(count)
+        runBlockingTest {
+            count.collect {
+                assertEquals(expected, it)
+            }
+        }
+    }
+
+     @Test
+     fun test_toggleToDo() {
+         val id = "fake"
+         val repository: ToDoRepository = mock()
+         val model = ToDoListViewModel(repository)
+
+         model.toggleToDo(id)
+
+         verify(repository)
+             .toggleTodo(id)
+     }
+
+    @Test
+    fun test_toggleToDoNotFound() {
+        val repository: ToDoRepository = mock()
+        val id = "fake"
+        val exceptionMessage = "Todo not found"
+
+        whenever(repository.toggleTodo(id))
+            .thenThrow(IllegalArgumentException(exceptionMessage))
+        val model = ToDoListViewModel(repository)
+        exceptionRule.expect(IllegalArgumentException::class.java)
+        exceptionRule.expectMessage(exceptionMessage)
         model.toggleToDo(id)
+
+        verify(repository)
+            .toggleTodo(id)
     }
 }
